1d2dd0df424186aa9003fd9f6a83dc7540a4a355
Petr Filip <petr.filip01@gmail.com>
2023-02-13 21:29:04 +0100

    Clicking improved

--- comgeo.py
+++ comgeo.py
@@ -14,30 +14,43 @@ class GeoArea(QtWidgets.QWidget):
         super().__init__()
         self.point = None
         self.pen = QtGui.QPen(QtGui.QColor("green"), 5)
-        #self.pen.setCapStyle = QtCore.Qt.PenCapStyle.RoundCap
-        #self.pixmap = QtGui.QPixmap(self.size())
-        self.pixmap = self.grab()
+        #self.pixmap = self.grab()
         self.image = QtGui.QImage()
 
     def paintEvent(self, pe):
-        if(self.point):
-            painter = QtGui.QPainter(self)
-            #painter.setPen(self.pen)
-            painter.drawPixmap(0,0,self.pixmap)
+        #if(not self.image.isNull()):
+        painter = QtGui.QPainter(self)
+        #painter.setPen(self.pen)
+        painter.drawImage(0,0,self.image)
     
     def mousePressEvent(self, event):
         if(event.button() == QtCore.Qt.LeftButton):
             self.createPoint(event.position())
 
-    #def resizeEvent(self, re):
-     #   pass
+    def resizeEvent(self, re):
+        if(self.width() > self.image.width() or self.height() > self.image.height()):
+            newWidth = max(self.width() + 128, self.image.width())
+            newHeight = max(self.height() + 128, self.image.height())
+            self.resizeImage(self.image, QtCore.QSize(newWidth, newHeight))
+            self.update()
 
     def createPoint(self, pos):
         self.point = QtCore.QPointF(pos)
-        painter = QtGui.QPainter(self.pixmap)
+        painter = QtGui.QPainter(self.image)
         painter.setPen(self.pen)
         painter.drawPoint(self.point)  
         self.update()
+
+    def resizeImage(self, oldImage, newSize):
+        if (self.image.size() == newSize):
+            return
+        
+        newImage = QtGui.QImage(newSize, QtGui.QImage.Format_RGB32)
+        newImage.fill(QtGui.QColor("white"))
+        painter = QtGui.QPainter(newImage)
+        
+        painter.drawImage(0, 0, oldImage)
+        self.image = newImage
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
